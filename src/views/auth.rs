721f74d90959568072f2e57751295895fe57b23f
interesnij <ochkarik1983@mail.com>
2023-12-16 19:49:20 +0300

    J

--- src/views/auth.rs
+++ src/views/auth.rs
@@ -415,7 +415,7 @@ pub async fn login_page(req: HttpRequest) -> actix_web::Result<HttpResponse> {
     if get_request_user(&req).is_some() {
         let request_user = get_request_user(&req).unwrap();
         if request_user.user_type == "client" {
-            return crate::views::client_overview_page(req).await;
+            return crate::views::client_overview_page(req);
         } 
         Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(request_user.key))
     }
