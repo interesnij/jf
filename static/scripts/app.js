e35f7070d2e9761570a66bd384979cb411603454
interesnij <ochkarik1983@mail.com>
2023-12-27 12:35:35 +0300

    J

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -144,4 +144,86 @@ on('body', 'click', '.menu-button', function() {
     block.style.opacity = "0";
     block.style.visibility = "hidden";
   }
-})
+});
+
+on('body', 'click', '#logg', function() {
+  _this = this;
+  form = _this.parentElement.parentElement; 
+  response1 = form.querySelector(".api_response1");
+  response2 = form.querySelector(".api_response2");
+
+  if (!form.querySelector("#id_email").value){
+    form.querySelector("#id_email").style.border = "1px #FF0000 solid";
+    response1.innerHTML = "Enter your email!";
+    response1.classList.add("error");
+    return
+  }
+  else if (!form.querySelector("#id_password").value){
+    form.querySelector("#id_password").style.border = "1px #FF0000 solid";
+    response2.innerHTML = "Enter the password!";
+    response2.classList.add("error")
+    return
+  }
+  else {
+    _this.disabled = true;
+  }
+
+  form_data = new FormData(form);
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'POST', "https://backend.juslaw.com/api/v1/auth/login/", true );
+  //link.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    //var jsonData = JSON.parse(link.response);
+    localStorage.setItem("request_data", link.response)
+    window.location.href = "/" 
+  }
+
+  else {
+    _this.disabled = false;
+    response.style.display = "block";
+    //response.innerHTML = "Логин или пароль - неверный!";
+    response.classList.add("error");
+    form.querySelector("#id_email").style.display = "block";
+    form.querySelector("#id_email").value = '';
+    form.querySelector("#id_password").value = '';
+  }};
+  link.send(form_data);
+});
+
+on('body', 'click', '#signup', function() {
+  _this = this;
+  form = _this.parentElement;
+  username = form.querySelector("#id_email");
+  response1 = form.querySelector(".api_response1");
+  response2 = form.querySelector(".api_response2");
+  if (!username.value){
+    username.style.border = "1px #FF0000 solid";
+    toast_error("Enter your email!");
+    return
+  } else if (!form.querySelector("#id_password").value){
+    form.querySelector("#id_password").style.border = "1px #FF0000 solid";
+    toast_error("Enter your password!");
+    return
+  }
+  else {
+    this.disabled = true
+  }
+
+  form_data = new FormData(form);
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'POST', "/signup/", true );
+
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    window.location.href = "/"
+    }
+  else {
+    _this.disabled = false;
+    response.style.display = "block";
+    response.innerHTML = "not ok";
+    response.classList.add("error");
+  }};
+  link.send(form_data);
+});
