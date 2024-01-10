0503960327452f8acecb89f63cc10265efc41348
interesnij <ochkarik1983@mail.com>
2024-01-10 21:14:06 +0300

    J

--- src/routes.rs
+++ src/routes.rs
@@ -3,6 +3,7 @@ use actix_web::web;
 use crate::views::{
     auth,
     clients,
+    attorneys,
     load,
 };
 
@@ -10,6 +11,7 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg
     .configure(auth::auth_routes)
     .configure(clients::clients_routes)
+    .configure(attorneys::attorneys_routes)
     .configure(load::load_routes)
     ;
 }
