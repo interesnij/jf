84deea30d375181012aac7abaae6e55f94dc0e6c
interesnij <ochkarik1983@mail.com>
2024-02-22 21:11:18 +0300

    J

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -1,3 +1,89 @@
+class ToastManager {
+  constructor() {
+      this.id = 0;
+      this.toasts = [];
+      this.icons = {
+          'SUCCESS': "",
+          'ERROR': '',
+          'INFO': '',
+          'WARNING': '',
+      };
+      var body = document.querySelector('body');
+      this.toastsContainer = document.createElement('div');
+      this.toastsContainer.classList.add('toasts', 'border-0');
+      body.appendChild(this.toastsContainer)
+  }
+  showSuccess(message) {
+      return this._showToast(message, 'SUCCESS')
+  }
+  showError(message) {
+      return this._showToast(message, 'ERROR')
+  }
+  showInfo(message) {
+      return this._showToast(message, 'INFO')
+  }
+  showWarning(message) {
+      return this._showToast(message, 'WARNING')
+  }
+  _showToast(message, toastType) {
+      var newId = this.id + 1;
+      var newToast = document.createElement('div');
+      newToast.style.display = 'inline-block';
+      newToast.classList.add(toastType.toLowerCase());
+      newToast.classList.add('toast');
+      newToast.innerHTML = `<progress max="100"value="0"></progress><h3>${message}</h3>`;
+      var newToastObject = {
+          id: newId,
+          message,
+          type: toastType,
+          timeout: 4000,
+          progressElement: newToast.querySelector('progress'),
+          counter: 0,
+          timer: setInterval(() => {
+              newToastObject.counter += 1000 / newToastObject.timeout;
+              newToastObject.progressElement.value = newToastObject.counter.toString();
+              if (newToastObject.counter >= 100) {
+                  newToast.style.display = 'none';
+                  clearInterval(newToastObject.timer);
+                  this.toasts = this.toasts.filter((toast) => {
+                      return toast.id === newToastObject.id
+                  })
+              }
+          }, 10)
+      }
+      newToast.addEventListener('click', () => {
+          newToast.style.display = 'none';
+          clearInterval(newToastObject.timer);
+          this.toasts = this.toasts.filter((toast) => {
+              return toast.id === newToastObject.id
+          })
+      });
+      this.toasts.push(newToastObject);
+      this.toastsContainer.appendChild(newToast);
+      return this.id++
+  }
+}
+
+function toast_success(text) {
+  var toasts = new ToastManager();
+  toasts.showSuccess(text)
+}
+
+function toast_error(text) {
+  var toasts = new ToastManager();
+  toasts.showError(text)
+}
+
+function toast_info(text) {
+  var toasts = new ToastManager();
+  toasts.showInfo(text)
+}
+
+function toast_warning(text) {
+  var toasts = new ToastManager();
+  toasts.showWarning(text)
+}
+
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});}
 
 $height = parseFloat(window.innerHeight * 0.000264).toFixed(2);
