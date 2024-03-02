d3133c87287ee2b729aa122e667002025df91c72
interesnij <ochkarik1983@mail.com>
2024-03-02 17:48:35 +0300

    J

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -34,11 +34,14 @@ fn get_data<'a>(req: &'a HttpRequest) -> Option<&'a str> {
 pub fn get_request_user(req: &HttpRequest) -> Option<AuthResponseData> {
     let _tokenize = get_data(req);
     if _tokenize.is_some() { 
+        println!("_tokenize exist!");
         let _user_res: Result<AuthResponseData, serde_json::Error> = serde_json::from_str(&_tokenize.unwrap());
         if _user_res.is_ok() {
             let _user: AuthResponseData = _user_res.expect("E.");
+            println!("_user ok!");
             return Some(_user);
         }
+        println!("_user errr......");
         return None;
     }
     return None;
